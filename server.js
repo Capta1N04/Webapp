4d49d52a5118728ecf2bbd05792f9d84cfff991a
Princeingh <minthukhawithabby@gmail.com>
2024-10-12 13:59:53 -0400

    updated version

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require("express");
 const path = require("path");
+const storeService = require("./store-service");
+
 const app = express();
-const HTTP_Port = process.env.PORT || 8080;
-const server_service = require("./store-service");
+const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("This is Main page");
-});
+storeService
+  .initialize()
+  .then(() => {
+    console.log("Data initialized successfully");
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/about.html"));
-});
+    app.get("/shop", async (req, res) => {
+      try {
+        const items = await storeService.getAllItems();
+        res.json(items);
+      } catch (error) {
+        console.error(error);
+        res.status(404).send("No items found");
+      }
+    });
 
-app.listen(HTTP_Port, () => {
-  console.log(`Server Listening on port ${HTTP_Port}`);
-});
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "views/about.html"));
+    });
+
+    app.get("/items", async (req, res) => {
+      try {
+        const items = await storeService.getAllItems();
+        res.json(items);
+      } catch (error) {
+        console.error(error);
+        res.status(404).send("No items found");
+      }
+    });
+
+    app.get("/categories", async (req, res) => {
+      try {
+        const categories = await storeService.getCategories();
+        res.json(categories);
+      } catch (error) {
+        console.error(error);
+        res.status(404).send("No categories found");
+      }
+    });
+
+    app.use((req, res) => {
+      res.status(404).sendFile(path.join(__dirname, "/views/404_error.html"));
+    });
+
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error initializing data:", err);
+  });
--- store-service.js
+++ store-service.js
@@ -1,1 +1,67 @@
-const store_server = require("./server");
+const fs = require("fs");
+const path = require("path");
+let items = [];
+let categories = [];
+
+function initialize() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const itemsData = fs.readFileSync(
+        path.join(__dirname, "data", "items.json"),
+        "utf8"
+      );
+
+      items = JSON.parse(itemsData);
+      console.log(items);
+
+      const CategoriesData = fs.readFileSync(
+        path.join(__dirname, "data", "categories.json"),
+        "utf8"
+      );
+      categories = JSON.parse(CategoriesData);
+
+      resolve();
+    } catch (error) {
+      reject("unable to read file");
+    }
+  });
+}
+
+function getAllItems() {
+  return new Promise((resolve, reject) => {
+    if (items.length === 0) {
+      reject("No results found");
+    } else {
+      resolve(items);
+    }
+  });
+}
+
+function getPublishedItems() {
+  return new Pormise((resolve, reject) => {
+    const publish = items.filter((item) => item.published === true);
+
+    if (publish.length > 0) {
+      resolve(publish);
+    } else {
+      reject("no results returned");
+    }
+  });
+}
+
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length === 0) {
+      reject("no results returned");
+    } else {
+      resolve(categories);
+    }
+  });
+}
+initialize();
+module.exports = {
+  initialize,
+  getAllItems,
+  getPublishedItems,
+  getCategories,
+};
